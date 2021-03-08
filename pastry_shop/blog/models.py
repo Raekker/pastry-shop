f2abcba3feae2ccdd81c3165703866078b3634d1
Raekker <maciej.purta@wp.pl>
2021-03-08 17:30:36 +0100

    add blog app and it's models

---
+++ pastry_shop/blog/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+from pastry_shop.users.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(_("Title"), max_length=64)
+    content = models.TextField(_("Content"))
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    comments = models.ForeignKey(
+        "Comment", on_delete=models.CASCADE, related_name="comments"
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    content = models.TextField(_("Content"))
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.content[:20]} - {self.author.username}"
