7a1aeb480da8f2128d57f8aeb72e992445e9e1e9
Raekker <maciej.purta@wp.pl>
2021-03-10 17:55:25 +0100

    shop add category list view

--- pastry_shop/shop/views.py
+++ pastry_shop/shop/views.py
@@ -9,7 +9,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 
 from pastry_shop.shop.forms import ProductForm
-from pastry_shop.shop.models import Product
+from pastry_shop.shop.models import Product, Category
 
 
 class ProductListView(ListView):
@@ -53,3 +53,9 @@ class ProductDeleteView(DeleteView):
     model = Product
     template_name = "shop/product_confirm_delete.html"
     success_url = reverse_lazy("shop:product-list")
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = "shop/category_list.html"
+    context_object_name = "categories"
