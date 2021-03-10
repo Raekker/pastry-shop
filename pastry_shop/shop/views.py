ed8b44c47730d9de705a50e24405b40f4acf97df
Raekker <maciej.purta@wp.pl>
2021-03-10 16:40:09 +0100

    shop add product detail view

--- pastry_shop/shop/views.py
+++ pastry_shop/shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from pastry_shop.shop.models import Product
@@ -11,3 +12,9 @@ class ProductListView(ListView):
     template_name = "shop/product_list.html"
     context_object_name = "products"
     paginate_by = 5
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "shop/product_detail.html"
+    context_object_name = "product"
