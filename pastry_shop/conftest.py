0e74d7baf0a5d121db901dea449b7ac68514ee75
Raekker <maciej.purta@wp.pl>
2021-03-17 14:02:24 +0100

    add tests for shop views

--- pastry_shop/conftest.py
+++ pastry_shop/conftest.py
@@ -1,8 +1,17 @@
-import pytest
+import pytest, random
 
 from faker import Faker
 
-from pastry_shop.shop.models import Product
+from pastry_shop.shop.models import (
+    Product,
+    Category,
+    Shop,
+    Cart,
+    ProductCart,
+    Order,
+    ProductOrder,
+    Availability,
+)
 from pastry_shop.users.models import User
 from pastry_shop.users.tests.factories import UserFactory
 from pastry_shop.blog.models import Post, Comment
@@ -29,8 +38,29 @@ def set_up():
         email="test@gmail.com",
         is_superuser=True,
     )
+    cart = Cart.objects.create(client=author)
     for _ in range(3):
         post = Post.objects.create(
             title=fake.word(), content=fake.text(), author=author
         )
         Comment.objects.create(author=author, content=fake.text(), post=post)
+        category = Category.objects.create(name=fake.word())
+        product = Product.objects.create(
+            name=fake.word(),
+            description=fake.text(),
+            price=random.random() * 10,
+            amount=fake.pyint(min_value=1),
+        )
+        shop = Shop.objects.create(
+            name=fake.word(), city=fake.city(), street=fake.address()
+        )
+        Availability.objects.create(
+            shop=shop, product=product, amount=fake.pyint(min_value=1)
+        )
+        product.categories.add(category)
+        ProductCart.objects.create(
+            cart=cart, product=product, amount=fake.pyint(min_value=1)
+        )
+    order = Order.objects.create(client=author, status=1)
+    for el in ProductCart.objects.filter(cart=cart):
+        ProductOrder.objects.create(order=order, product=el.product, amount=el.amount)
